96274439c7290702ab1c0f7b69ff08862b3cb2a8
Nair00 <cipri223223@gmail.com>
2023-04-22 18:24:47 +0300

    SignIn page

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,15 +16,9 @@ export default function Home() {
   const signInWithGoogle = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential?.accessToken;
-        // The signed-in user info.
         const user = result.user;
-        console.log(result);
-        // setUsername(result.user.displayName ?? 'nume');
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
       })
       .catch((error) => {
         setUsername('Eroare');
