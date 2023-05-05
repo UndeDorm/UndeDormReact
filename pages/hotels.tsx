2b97724996d9dd0a0bcdce680040bad14cb8181e
Bogdan <bogdan_bbx@yahoo.com>
2023-05-05 15:20:04 +0300

    print hotel list

--- pages/hotels.tsx
+++ pages/hotels.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../src/providers/auth/AuthProvider';
 import { useRouter } from 'next/router';
 import { firebaseDb } from '../src/firebase/firebase';
@@ -11,17 +11,21 @@ export default function HotelList() {
   const router = useRouter();
   const hotelsRef = collection(firebaseDb, 'hotels');
 
+  const [hotelNames, setHotelNames] = useState<string[]>([]);
+
   useEffect(() => {
     if (!state.isUserLoggedIn) {
-      console.log('You are not logged in!')
+      console.log('You are not logged in!');
       router.push('/');
     } else {
       const getHotels = async () => {
         const hotelsSnapshot = await getDocs(hotelsRef);
-        hotelsSnapshot.forEach((hotelDoc) => {
-          console.log(hotelDoc.data());
-        });
-      }
+        const newHotelNames = hotelsSnapshot.docs
+          .map((doc) => doc.data().Name)
+          .filter((Name) => Name !== undefined && Name !== null);
+
+        setHotelNames(newHotelNames);
+      };
       getHotels();
     }
   }, [state.isUserLoggedIn, router, hotelsRef]);
@@ -36,6 +40,11 @@ export default function HotelList() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Cautare de hoteluri</h1>
+        <ul>
+          {hotelNames.map((name) => (
+            <li key={name}>{name}</li>
+          ))}
+        </ul>
       </main>
     </div>
   );
