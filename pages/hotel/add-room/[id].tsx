dd9a42d4f6152a5a2808232d66b9523e48c1949b
Bogdan <bogdan_bbx@yahoo.com>
2023-05-11 10:31:48 +0300

    fix

--- pages/hotel/add-room/[id].tsx
+++ pages/hotel/add-room/[id].tsx
@@ -1,15 +1,15 @@
 import { collection, doc, getDoc } from 'firebase/firestore';
 import router from 'next/router';
 import { useContext, useEffect, useRef, useState } from 'react';
-import { addRoom } from '../../../src/firebase/database';
+import { addRoom, getHotel } from '../../../src/firebase/database';
 import { firebaseDb } from '../../../src/firebase/firebase';
 import { AuthContext } from '../../../src/providers/auth/AuthProvider';
-import { Room } from '../../../src/utils/types';
+import { Hotel, Room } from '../../../src/utils/types';
 import styles from '../../../styles/Home.module.css';
+import { error } from 'console';
 
 export default function AddRoomPage({ id }: { id: string }) {
   const { state } = useContext(AuthContext);
-  const [hotelOwnerId, setHotelOwnerId] = useState<string>();
   const roomName = useRef<string>('');
   const myCollection = collection(firebaseDb, 'rooms');
   const myDocRef = doc(myCollection);
@@ -17,32 +17,27 @@ export default function AddRoomPage({ id }: { id: string }) {
   const price = useRef<number>();
   const benefits = useRef<string>();
 
+  const hotelOwnerId = useRef<string>();
+  const hotelData = useRef<Hotel>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
   useEffect(() => {
     if (!state.isUserLoggedIn) {
       console.log('You are not logged in!');
       router.push('/');
       return;
+    } else {
+      getHotel(id)
+        .then((data) => {
+          hotelData.current = data;
+          hotelOwnerId.current = data.ownerId;
+        })
+        .catch((error) => {
+          console.log('Error getting hotel:', error);
+        })
+        .finally(() => setIsLoading(false));
     }
-
-    async function fetchHotel() {
-      const hotelRef = doc(firebaseDb, 'hotels', id);
-      const hotelSnapshot = await getDoc(hotelRef);
-
-      if (hotelSnapshot.exists()) {
-        const hotelData = hotelSnapshot.data();
-        setHotelOwnerId(hotelData?.ownerId);
-        if (hotelOwnerId != null && hotelOwnerId !== state.user?.id) {
-          console.log('You are not the owner of this hotel!');
-          router.push('/');
-        }
-      } else {
-        console.log('Hotel not found!');
-        router.push('/');
-      }
-    }
-
-    fetchHotel();
-  }, [hotelOwnerId, id, state]);
+  }, [id, state.isUserLoggedIn]);
 
   const addRoomToDatabase = () => {
     const room: Room = {
@@ -66,32 +61,44 @@ export default function AddRoomPage({ id }: { id: string }) {
     addRoom({ room, onSuccess, onFailure });
   };
 
+  const renderPage = () => {
+    return (
+      <>
+        <h1 className={styles.title}>Add a room</h1>
+        <input
+          placeholder="Name"
+          onChange={(e) => (roomName.current = e.target.value)}
+          className={styles.input}
+        />
+        <input
+          placeholder="Number of beds"
+          onChange={(e) => (noBeds.current = parseInt(e.target.value))}
+          className={styles.input}
+        />
+        <input
+          placeholder="Price per night"
+          className={styles.input}
+          onChange={(e) => (price.current = parseInt(e.target.value))}
+        />
+        <input
+          placeholder="Benefits"
+          className={styles.input}
+          onChange={(e) => (benefits.current = e.target.value)}
+        />
+        <button className={styles.card} onClick={addRoomToDatabase}>
+          Add Room
+        </button>
+      </>
+    );
+  };
+
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Add a room</h1>
-      <input
-        placeholder="Name"
-        onChange={(e) => (roomName.current = e.target.value)}
-        className={styles.input}
-      />
-      <input
-        placeholder="Number of beds"
-        onChange={(e) => (noBeds.current = parseInt(e.target.value))}
-        className={styles.input}
-      />
-      <input
-        placeholder="Price per night"
-        className={styles.input}
-        onChange={(e) => (price.current = parseInt(e.target.value))}
-      />
-      <input
-        placeholder="Benefits"
-        className={styles.input}
-        onChange={(e) => (benefits.current = e.target.value)}
-      />
-      <button className={styles.card} onClick={addRoomToDatabase}>
-        Add Room
-      </button>
+      {!isLoading ? (
+        renderPage()
+      ) : (
+        <h1 className={styles.title}>{'Loading...'}</h1>
+      )}
     </main>
   );
 }
