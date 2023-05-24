8d08a3ab65833302215d8275c145abf6b9e73308
Buhnici Andrei <andreibuhnici@gmail.com>
2023-05-24 15:33:14 +0300

    Revert "Feat/images"

--- pages/hotel/modify-room/[id].tsx
+++ pages/hotel/modify-room/[id].tsx
@@ -1,4 +1,3 @@
-import { deleteObject, getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useContext, useEffect, useRef, useState } from 'react';
@@ -19,9 +18,6 @@ export default function ModifyRoom({ id }: { id: string }) {
   const hotelData = useRef<Hotel>();
   const hotelOwnerId = useRef<string>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const imageURLs = useRef<string[]>([]);
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [showDeleteButton, setShowDeleteButton] = useState(false);
 
   useEffect(() => {
     if (!state.isUserLoggedIn) {
@@ -44,23 +40,12 @@ export default function ModifyRoom({ id }: { id: string }) {
                 hotelData.current = data;
                 hotelOwnerId.current = data.ownerId ?? '';
               })
-          }
-
-          if (roomData.current?.images && roomData.current?.images.length > 0) {
-            const imageUrlsPromises = roomData.current?.images.map((imageId) => {
-              const imageRef = ref(storage, `rooms/${id}/${imageId}`);
-              return getDownloadURL(imageRef);
-            });
-    
-            Promise.all(imageUrlsPromises)
-              .then((imageUrls) => {
-                imageURLs.current = imageUrls;
-                setIsLoading(false);
-              })
               .catch((error) => {
-                console.error('Error getting image URLs', error);
-                setIsLoading(false);
+                console.log('Error getting hotel:', error);
               })
+              .finally(() => {
+                setIsLoading(false);
+              });
           } else {
             setIsLoading(false);
           }
@@ -128,122 +113,33 @@ export default function ModifyRoom({ id }: { id: string }) {
   };
 
   const renderPage = () => {
-    const handlePreviousImage = () => {
-      setCurrentImageIndex((prevIndex) =>
-        prevIndex === 0 ? imageURLs.current.length - 1 : prevIndex - 1
-      );
-    };
-  
-    const handleNextImage = () => {
-      setCurrentImageIndex((prevIndex) =>
-        prevIndex === imageURLs.current.length - 1 ? 0 : prevIndex + 1
-      );
-    };
-
-    const handleDeleteImage = () => {
-      const imageId = roomData.current?.images[currentImageIndex];
-      const imageRef = ref(storage, `rooms/${id}/${imageId}`);
-      const updatedImages = [...roomData.current?.images || []];
-      updatedImages.splice(currentImageIndex, 1);
-
-      editRoom({
-        roomId: id,
-        newData: { images: updatedImages },
-        onSuccess: () => {},
-        onFailure: (error) => {
-          console.error('Error deleting image:', error);
-        }
-      })
-      
-      deleteObject(imageRef).then(() => {
-        alert('Image deleted successfully!');
-        router.reload();
-      }).catch((error) => {
-        console.error('Error deleting image:', error);
-      });
-    }
-
     return (
       <>
         <h1>{'Room name'}</h1>
         <input
           type="text"
           defaultValue={roomOriginalData.current?.name}
-          className={styles.input}
           onChange={(e) => (roomNameRef.current = e.target.value)}
         />
         <h1>{'Room number of beds'}</h1>
         <input
           type="number"
           defaultValue={roomOriginalData.current?.beds}
-          className={styles.input}
           onChange={(e) => (roomNoBedsRef.current = parseInt(e.target.value))}
         />
         <h1>{'Room price'}</h1>
         <input
           type="number"
           defaultValue={roomOriginalData.current?.pricePerNight}
-          className={styles.input}
           onChange={(e) => (roomPriceRef.current = parseInt(e.target.value))}
         />
         <h1>{'Room benefits'}</h1>
         <input
           type="text"
           defaultValue={roomOriginalData.current?.benefits}
-          className={styles.input}
           onChange={(e) => (roomBenefitsRef.current = e.target.value)}
         />
-        {(roomData.current?.images && roomData.current?.images.length > 0) ? (
-        <div className={styles.imageContainer}>
-          <button className={styles.card} onClick={handlePreviousImage}>
-            {'Previous'}
-          </button>
-          <div style={{ position: 'relative', display: 'inline-block' }}>
-            <img
-              className={styles.image}
-              src={imageURLs.current[currentImageIndex]}
-              onMouseEnter={() => setShowDeleteButton(true)}
-              onMouseLeave={() => setShowDeleteButton(false)}
-            />
-            {showDeleteButton && (
-              <div
-                style={{
-                  position: 'absolute',
-                  top: '50%',
-                  left: '50%',
-                  transform: 'translate(-50%, -50%)',
-                  zIndex: 1,
-                }}
-              >
-                <button onClick={handleDeleteImage}
-                className={styles.card}
-                onMouseEnter={() => setShowDeleteButton(true)}>
-                  {'Delete'}
-                </button>
-              </div>
-            )}
-          </div>
-          <button className={styles.card} onClick={handleNextImage}>
-            {'Next'}
-          </button>
-        </div>
-        ) : (
-          <h1>{'No images available.'}</h1>
-        )}
-        <input
-            type="file"
-            onChange={(e) => {
-              setImageUpload(e.target.files?.[0] ?? null);
-            }}
-            className={styles.input}
-          />
-
-        <button onClick={addImage} className={styles.card}>
-          {'Upload Image'}
-        </button>
-        <button onClick={onSave} className={styles.card}>
-          {'Save'}
-        </button>
+        <button onClick={onSave}>Save</button>
       </>
     );
   };
@@ -263,7 +159,10 @@ export default function ModifyRoom({ id }: { id: string }) {
             <h1 className={styles.title}>{'Loading...'}</h1>
           ) : (
             <>
-              {renderPage()}
+              {state.user?.isOwner &&
+              state.user.id === hotelData.current?.ownerId
+                ? renderPage()
+                : router.push('/')}
             </>
           )}
         </>
